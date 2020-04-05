8477bb3ed17f971d1359a1df13ac64ca53f6865d
CharlesOnesti <35116629+CharlesOnesti@users.noreply.github.com>
2020-04-05 16:13:44 -0400

    pass

--- proj.r
+++ proj.r
@@ -1,1 +1,13 @@
 hpop <- read.csv("Spring 19 data.csv")
+
+education_levels <- unique(hpop$Education..Categorical.)
+
+
+
+hs <- subset(hpop, hpop$Education..Categorical. == "High school")
+lesshs <- subset(hpop, hpop$Education..Categorical. == "Less than high school")
+someColl <-subset(hpop, hpop$Education..Categorical. == "Some college")
+bacc <- subset(hpop, hpop$Education..Categorical. == "Bachelor's degree or higher")
+
+model <- lm()
+
