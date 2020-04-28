b6ad8213c3bd2a7d4277a448c16ba1b55b01865c
CharlesOnesti <35116629+CharlesOnesti@users.noreply.github.com>
2020-04-28 14:05:23 -0400

    updated

--- proj.r
+++ proj.r
@@ -11,8 +11,25 @@ names(hpop)[names(hpop) == "X.Second.most.important..Which.of.the.following.do.y
 
 hpop_clean = subset(hpop, select = c(education, income, urbanization, climate_opinion, first, second))
 
-education_levels <- unique(hpop_clean$Education..Categorical.)
+hpop_clean$climate <- ifelse(hpop_clean['climate_opinion'] == "It's a crisis and demands urgent action", 2, ifelse(hpop_clean['climate_opinion'] == "It's a problem, but we don't need urgent action now; other issues are more important", 1, 0))
 
+hpop_clean$income_category <- ifelse(hpop_clean['income'] == 'Less than $5,000' |
+                                       hpop_clean['income'] == '$5,000 to $7,499' |
+                                       hpop_clean['income'] == '$7,500 to $9,999' |
+                                       hpop_clean['income'] == '$10,000 to $12,499' |
+                                       hpop_clean['income'] == '$12,500 to $14,999' |
+                                       hpop_clean['income'] == '$15,000 to $19,999' |
+                                       hpop_clean['income'] == '$20,000 to $24,999' |
+                                       hpop_clean['income'] == '$25,000 to $29,999' |
+                                       hpop_clean['income'] == '$30,000 to $34,999' |
+                                       hpop_clean['income'] == '$35,000 to $39,999',
+                                       'lower', ifelse(hpop_clean['income'] == '$40,000 to $49,999' |
+                                                   hpop_clean['income'] == '$50,000 to $59,999' |
+                                                   hpop_clean['income'] == '$60,000 to $74,999' |
+                                                   hpop_clean['income'] == '$75,000 to $84,999' |
+                                                   hpop_clean['income'] == '$85,000 to $99,999' |
+                                                   hpop_clean['income'] == '$100,000 to $124,999' |
+                                                   hpop_clean['income'] == '$125,000 to $149,999', 'middle', 'upper'))
 
 
 hs <- subset(hpop_clean, hpop_clean$education == "High school")
@@ -22,19 +39,22 @@ bacc <- subset(hpop_clean, hpop_clean$education == "Bachelor's degree or higher"
 
 model <- lm(climate ~ 
               education + 
-              income + 
+              income_category + 
               urbanization, data = hpop_clean, na.action = na.omit)
 coef(model)
 summary(model)
 
+unique(hpop_clean$urbanization)
+unique(hpop_clean$income_category)
+unique(hpop_clean$education)
+
 # plot(NA, NA, xlim = c(0,1), ylim = c(0,1))
 # abline(model)
 
-hpop_clean$climate <- ifelse(hpop_clean['climate_opinion'] == "It's a crisis and demands urgent action", 3, ifelse(hpop_clean['climate_opinion'] == "It's a problem, but we don't need urgent action now; other issues are more important", 2, 0))
 
-averages_education <- c('Less than HS diploma' = mean(lesshs$climate), 'HS Diploma' = mean(hs$climate), 'Some College' = mean(someColl$climate), 'Bachelor\'s degree or higher' = mean(bacc$climate))
+averages_education <- c('Less than HS diploma' = mean(lesshs$climate), 'HS Diploma' = mean(hs$climate), 'Some College' = mean(someColl$climate), 'Bachelor\'s or Higher' = mean(bacc$climate))
 
 
-barplot(xlab = 'Education Level', ylab = 'Climate Attitude', averages_education)
+barplot(xlab = 'Education Level', ylab = 'Climate Attitude', ylim = c(0,2), averages_education)
 
 
